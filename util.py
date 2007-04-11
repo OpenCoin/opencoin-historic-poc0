0bc09d68db3d800397eed7e355d68a6fb3bc0d6f
ntoedtma <ntoedtma@4cea9737-cb27-0410-bd70-f0a123989a92>
2007-04-11 15:17:17 +0000

    partition

--- util.py
+++ util.py
@@ -1,4 +1,14 @@
 """Utility functions"""
 
 
+def partition(value, denominations):
+    denominations.sort()
+    smallest=denominations[0]
+
+    part = []
+    rest = value
+
+    while rest > 0 :
+	denominations = [i for i in denominations if i <= rest/2]
+        n = 
 
