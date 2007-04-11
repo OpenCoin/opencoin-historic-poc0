c186976e821859286ff940965be1eaca01a710d8
ntoedtma <ntoedtma@4cea9737-cb27-0410-bd70-f0a123989a92>
2007-04-11 15:34:06 +0000

    partition

--- util.py
+++ util.py
@@ -2,6 +2,10 @@
 
 
 def partition(value, denominations):
+    """
+    Partition an integer into smaller summands from a given 
+    list of possible summands. Return list of summands
+    """
     denominations.sort()
     smallest=denominations[0]
 
@@ -10,5 +14,13 @@ def partition(value, denominations):
 
     while rest > 0 :
 	denominations = [i for i in denominations if i <= rest/2]
-        n = 
+	if denominations == [] : break
+        p = max (denominations)
+	part.append(p)
+	rest -= p
 
+    while rest >= smallest :
+	part.append(smallest)
+	rest -= smallest
+
+    return (part, rest)
